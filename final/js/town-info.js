913c3dbf2b5693b3adb2c1535d233a6d9b38a341
meyerstn.github.io <meyerstn@byui.edu>
2022-07-20 23:10:50 -0500

    final proj additions

---
+++ final/js/town-info.js
@@ -0,0 +1,20 @@
+"use strict"
+const webJSON = "data/towns.json"
+async function populateCityInfo(webJSON){
+    const response = await fetch(webJSON)
+    const townsJSON = await response.json()
+    const towns = townsJSON.towns
+    document.getElementById("playa-name").textContent = towns[6].name
+    document.getElementById("playa-slogan").textContent = towns[6].motto
+    document.getElementById("playa-year").textContent = towns[6].yearFounded
+    document.getElementById("playa-pop").textContent = towns[6].currentPopulation
+    document.getElementById("playa-rain").textContent = towns[6].averageRainfall
+    document.getElementById("puerta-name").textContent = towns[0].name
+    document.getElementById("puerta-slogan").textContent = towns[0].motto
+    document.getElementById("puerta-year").textContent = towns[0].yearFounded
+    document.getElementById("puerta-pop").textContent = towns[0].currentPopulation
+    document.getElementById("puerta-rain").textContent = towns[0].averageRainfall
+     document.getElementById("playa-img").setAttribute("alt", towns[6].name)
+    document.getElementById("puerta-img").setAttribute("alt", towns[0].name)
+ 
+populateCityInfo(webJSON)
--- weather/js/weatherapi.js
+++ weather/js/weatherapi.js
@@ -1,1 +1,2 @@
 "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=85bd60f2bcadee1928837833e496d79e";
+document.getElementById('todayimg').src = 'https://openweathermap.org/img/w/' + icon + '.png';
